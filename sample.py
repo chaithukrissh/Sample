47d3df538e41b8415be4c1488c3c1b5239316f38
chaithu <chaithanya.thaticherla@gmail.com>
2023-04-03 15:31:39 +0530

    this is voting file

---
+++ sample.py
@@ -0,0 +1,16 @@
+n = input("enter any number")
+
+
+def Eligibility(x):
+    if x>=18:
+        return f"you are eligible for voting"
+    else:
+        return f"Go man !!! Study first"
+
+
+print(Eligibility(n))
+
+
+
+
+
